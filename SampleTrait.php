c574f988874948889a0974a4d696f5c7112e1bce
alyouma <alyoumaakmal4@gmail.com>
2025-04-15 18:33:37 +0700

    validation function

---
+++ SampleTrait.php
@@ -0,0 +1,18 @@
+<?php
+
+
+trait SampleTrait
+{
+
+    public abstract function sampleFunction(string $name): string;
+}
+
+class SampleTraitImpl
+{
+    use SampleTrait;
+
+    public function sampleFunction(int $name): int
+    {
+        return 1;
+    }
+}
