c57597a306490f925d6736de86605d4cb4147f79
alyouma <alyoumaakmal4@gmail.com>
2025-04-16 02:09:34 +0700

    new-string

---
+++ string.php
@@ -0,0 +1,13 @@
+<?php
+
+
+var_dump(str_contains("Eko Kurniawan Khannedy", "Eko"));
+var_dump(str_contains("Eko Kurniawan Khannedy", "Kurniawan"));
+
+
+var_dump(str_starts_with("Eko Kurniawan", "Eko"));
+var_dump(str_starts_with("Eko Kurniawan", "Kurniawan"));
+
+
+var_dump(str_ends_with("Eko Kurniawan", 'Eko'));
+var_dump(str_ends_with("Eko Kurniawan", 'Kurniawan'));
